6238d507f5f9f92034bea8282e3608f955197c35
masterhwan <mastarhwan@gmail.com>
2018-07-10 14:39:51 +0900

    add logout

--- src/main/java/com/chook9/board/controller/LoginController.java
+++ src/main/java/com/chook9/board/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.chook9.board.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,5 +11,11 @@ public class LoginController {
 	@GetMapping("/loginFailed")
 	public String failed() {
 		return "/user/login_failed";
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpSession httpSession) {
+		httpSession.removeAttribute("loginedUser");
+		return String.format("redirect:/");
 	}
 }
--- src/main/java/com/chook9/board/domain/KeysRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chook9.board.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface KeysRepository extends CrudRepository<Keys, Long> {
-
-}
